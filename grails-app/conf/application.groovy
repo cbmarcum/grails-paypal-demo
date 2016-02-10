af61f0dba8c8f8a8ce4d16433246a1ae48e0a8d1
mansiarora <mansi@intelligrape.com>
2016-02-10 16:57:51 +0530

    updated server url;

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -2,6 +2,6 @@ environments {
     development {
         grails.paypal.server = "https://www.sandbox.paypal.com/cgi-bin/webscr"
         grails.paypal.email = "testpp_1211202427_biz@g2one.com"
-        grails.serverURL = "http://812.99.101.131"
+        grails.serverURL = "http://216.113.188.202"
     }
 }
