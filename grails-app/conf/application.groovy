8352347bf721aa4cb896c4bb9b313316228d404b
mansiarora <mansi@intelligrape.com>
2016-02-10 16:47:06 +0530

    Added paypal configurations;

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,7 @@
+environments {
+    development {
+        grails.paypal.server = "https://www.sandbox.paypal.com/cgi-bin/webscr"
+        grails.paypal.email = "testpp_1211202427_biz@g2one.com"
+        grails.serverURL = "http://812.99.101.131"
+    }
+}
