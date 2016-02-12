83fc96be456c17c0a0ea853f00e100b1dafbb721
mansiarora <mansi@intelligrape.com>
2016-02-12 13:44:57 +0530

    Added bootstrap data;

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,18 @@
+import grails.paypal.demo.Customer
+import grails.paypal.demo.ProductItem
+
 class BootStrap {
 
     def init = { servletContext ->
+        Random ran = new Random()
+        if (!ProductItem.count()) {
+            (1..5).each {
+                new ProductItem(name: "Item $it", price: ran.nextInt(1000).toBigDecimal()).save(flush: true)
+            }
+        }
+        if (!Customer.count()) {
+            new Customer(firstName: "John", lastName: 'Doe', email: 'john.doe@gmail.com').save(flush: true)
+        }
     }
     def destroy = {
     }
